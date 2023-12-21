3f9d677e0cab3eaf9a5fe4f357efd41dfff838e3
YPYP333YPYP <ypyp333ypyp@gmail.com>
2023-12-21 16:50:52 +0900

    :sparkles: 파일, 이미지 채팅 보내기 기능

--- api/chat.py
+++ api/chat.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
+from typing import Union
 
 from broadcaster import Broadcast
 from fastapi import APIRouter, HTTPException, WebSocket, Depends, status, Query, Header
-from pydantic import BaseModel
+from pydantic import BaseModel, AnyHttpUrl
 from sqlalchemy.orm import Session
 from starlette.websockets import WebSocketDisconnect
 from api.user import get_current_user, get_current_user_from_parameter
@@ -13,7 +14,6 @@ from models import ChatRoom, Membership, User, Message
 router = APIRouter()
 
 broadcast = Broadcast("redis://localhost:6379")
-CHANNEL = "CHAT"
 
 
 def get_db():
@@ -27,20 +27,33 @@ def get_db():
 class MessageEvent(BaseModel):
     user_id: int
     message: str
+    file_url: AnyHttpUrl = None
 
 
-async def receive_message(websocket: WebSocket):
-    async with broadcast.subscribe(channel=CHANNEL) as subscriber:
+async def receive_message(websocket: WebSocket, room_id: int):
+    async with broadcast.subscribe(channel=str(room_id)) as subscriber:
         async for event in subscriber:
             message_event = MessageEvent.model_validate_json(event.message)
             await websocket.send_json(message_event.model_dump_json())
 
 
-async def send_message(websocket: WebSocket, user_id: int):
+def save_message_to_database(db: Session, message_event: MessageEvent, room_id: int):
+    message = Message(user_id=message_event.user_id, content=message_event.message, room_id=room_id)
+    db.add(message)
+    db.commit()
+
+
+async def send_message(websocket: WebSocket,
+                       user_id: int,
+                       room_id: int,
+                       message: Union[str, bytes],
+                       db: Session,
+                       file_url: AnyHttpUrl = None,
+                       ):
     data = await websocket.receive_text()
-    event = MessageEvent(user_id=user_id, message=data)
-    await broadcast.publish(channel=CHANNEL, message=event.model_dump_json())
-    return event
+    event = MessageEvent(user_id=user_id, message=message, file_url=file_url)
+    save_message_to_database(db, event, room_id)
+    await broadcast.publish(channel=str(room_id), message=event.model_dump_json())
 
 
 @router.websocket("/ws/{room_id}/{user_id}")
@@ -57,21 +70,22 @@ async def chat_ws(
     try:
         while True:
             # 채팅 메시지 수신
-
             receive_message_task = asyncio.create_task(
-                receive_message(websocket)
+                receive_message(websocket, room_id)
             )
 
-            data = send_message(websocket, user_id)
-            print(data)
+            send_message_task = asyncio.create_task(
+                send_message(websocket, user_id, room_id, await websocket.receive_bytes(), db)
+            )
 
-            send_message_task = asyncio.create_task(send_message(websocket, user_id))
             done, pending = await asyncio.wait(
                 {receive_message_task, send_message_task},
                 return_when=asyncio.FIRST_COMPLETED,
             )
+
             for task in pending:
                 task.cancel()
+
             for task in done:
                 task.result()
     except WebSocketDisconnect:
